e6f0fc530cb68964aa439ba85327719cf1dc62f3
Eshu Goel <eshug@exzeo.com>
2016-11-11 12:02:57 +0530

    Implement the instance creation functionality

--- index.js
+++ index.js
@@ -3,14 +3,21 @@
 var main = require("./lib/main");
 module.exports = main;
 
-/*main.configure({
+main.configure({
     CONFIG_SERVICE_URL : "http://172.16.2.177:8080"
 });
 
-main.deployModel("modelFile")
+/*main.deployModel("modelFile")
 .then(response=>{
+    main.createInstance()
+    .then(response=>{
 
+    })
+    .catch(error=>{
+        
+    });
 })
 .catch(error=>{
     
 });*/
+
